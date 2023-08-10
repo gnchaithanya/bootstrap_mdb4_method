b43c936bc770495a053ad3919f84b3249b84733e
gnchaithanya <chaithugajjala28@gmail.com>
2023-08-10 13:48:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdb4(request):
+    return render(request,'mdb4.html')
+def modal(request):
+    return render(request,'modal.html')
+def dropdowns(request):
+    return render(request,'dropdowns.html')
+def spinners(request):
+    return render(request,'spinners.html')
+def pills(request):
+    return render(request,'pills.html')
+def pagination(request):
+    return render(request,'pagination.html')
+def stepper(request):
+    return render(request,'stepper.html')
+def carousels(request):
+    return render(request,'carousels.html')
+def cards(request):
+    return render(request,'cards.html')
